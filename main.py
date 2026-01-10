572e0134ad955dde1cdd9fb0038c0d36569ae994
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2026-01-10 11:50:03 +0100

    added min_value and max_value

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+# Ask min and max
+# start loop
+
 import random
 
 computer_choice = random.randint(1, 100)
 number_of_guesses = 0
-print(computer_choice)
+
+min_value = int(input("Please enter the lowest possible number: "))
+max_value = int(input("Please enter the highest possible number: "))
+
 while True:
     try:
         user_input = int(input('Guess the number (between 1 and 100): '))
@@ -11,13 +17,14 @@ while True:
         continue
     if user_input <= 0 or user_input > 100:
         print('Please enter a number between 1 and 100.')
-    elif user_input == computer_choice:
-        number_of_guesses += 1 
+        continue
+    
+    number_of_guesses += 1
+
+    if user_input == computer_choice:
         print(f"Congratulations! You guessed the number in {number_of_guesses} attempt{'s' if number_of_guesses != 1 else ''}.")
         break
     elif user_input > computer_choice:
         print('Too high! Try again.')
-        number_of_guesses += 1
     elif user_input < computer_choice:
         print('Too low! Try again.')
-        number_of_guesses += 1
