7b36742bcb3df19a905cb3b996b9bb9016cc1a4c
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2026-01-10 11:40:20 +0100

    Added logic for user input < 0

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ number_of_guesses = 1
 print(computer_choice)
 while True:
     try:
-        user_input = int(input("Guess the number (between 1 and 100): "))
+        user_input = int(input('Guess the number (between 1 and 100): '))
     except ValueError:
-        print("Please enter a number.")
+        print('Please enter a number.')
         continue
-    if user_input == computer_choice:
-        print(f"Congratulations! You guessed the number in {number_of_guesses} attempts.")
+    if user_input <= 0:
+        print('Please enter a number between 1 and 100.')
+    elif user_input == computer_choice:
+        print(f'Congratulations! You guessed the number in {number_of_guesses} attempt{'s' if number_of_guesses != 1 else ''}.')
         break
     elif user_input > computer_choice:
-        print("Too high! Try again.")
+        print('Too high! Try again.')
         number_of_guesses += 1
     elif user_input < computer_choice:
-        print("Too low! Try again.")
+        print('Too low! Try again.')
         number_of_guesses += 1
