4a6e76f9f90fb1584095f723a96638116a9cc1c7
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2026-01-10 11:33:12 +0100

    Added logic

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-# computer selects 1 - 100
-# loop
-# Ask: guess the number
-#     if user input too high:
-#         print too high
-#     if user input too low:
-#         print too low
-#     if user input == computer num:
-#         print congratualtion
+import random
+
+computer_choice = random.randint(1, 100)
+number_of_guesses = 1 
+
+while True:
+    user_input = int(input("Guess the number (between 1 and 100): "))
+    if user_input == computer_choice:
+        print(f"Congratulations! You guessed the number in {number_of_guesses} attempts.")
+        break
+    elif user_input > computer_choice:
+        print("Too high! Try again.")
+        number_of_guesses += 1
+    elif user_input < computer_choice:
+        print("Too low! Try again.")
+        number_of_guesses += 1
