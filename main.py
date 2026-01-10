a57a50e8dc8c3a28d7a186e1f91df5d27223ee59
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2026-01-10 11:26:27 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# computer selects 1 - 100
+# loop
+# Ask: guess the number
+#     if user input too high:
+#         print too high
+#     if user input too low:
+#         print too low
+#     if user input == computer num:
+#         print congratualtion
