b00a7ab39bac1b0c210b21c3681ace39f4d9cdb2
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2026-01-11 13:58:28 +0100

    Added best score variable

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-# Ask min and max
-# start loop
-
 import random
-
-computer_choice = random.randint(1, 100)
-number_of_guesses = 0
 
 min_value = int(input("Please enter the lowest possible number: "))
 max_value = int(input("Please enter the highest possible number: "))
+computer_choice = random.randint(min_value, max_value)
+number_of_guesses = 0
+number_of_guesses_limit = 10
+best_score = number_of_guesses_limit
+print(f"\nYou have {number_of_guesses_limit} attempts.\n")
+
 
 while True:
     try:
-        user_input = int(input('Guess the number (between 1 and 100): '))
+        user_input = int(
+            input(f"Guess the number (between {min_value} and {max_value}): ")
+        )
     except ValueError:
-        print('Please enter a number.')
+        print("Please enter a number.")
         continue
-    if user_input <= 0 or user_input > 100:
-        print('Please enter a number between 1 and 100.')
+
+    if user_input < min_value or user_input > max_value:
+        print(f"Please enter a number between {min_value} and {max_value}.")
         continue
-    
+
     number_of_guesses += 1
+    if number_of_guesses >= number_of_guesses_limit:
+        print(f"You ran out of attempts! The number was {computer_choice}.")
+        break
 
     if user_input == computer_choice:
         print(f"Congratulations! You guessed the number in {number_of_guesses} attempt{'s' if number_of_guesses != 1 else ''}.")
+        if number_of_guesses < best_score:
+            best_score = number_of_guesses
+            print('New best score!')
+        print(f"The current best score is {best_score} guess{'es' if best_score != 1 else ''}.")
         break
     elif user_input > computer_choice:
-        print('Too high! Try again.')
+        print("Too high! Try again.")
     elif user_input < computer_choice:
-        print('Too low! Try again.')
+        print("Too low! Try again.")
